02ea99de951117f03298afa3b77529e6e1fbb526
Robert Yang <yang.robert.w@gmail.com>
2016-09-15 12:19:51 -0400

    week02a, intro to code

---
+++ week02/Assets/scripts/CubeControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl: MonoBehaviour {
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// move forward if I press W
+		if ( Input.GetKey(KeyCode.W) ) {
+			// Translate, by default, converts to local space for us
+			transform.Translate( 0f, 0f, 10f * Time.deltaTime ); // will always move based on Cube's forward
+
+			// commented-out: because it moves in World Space instead of Local
+		//	transform.position += new Vector3( 0f, 0f, 1f );
+		//	transform.position.z += 1f; // <--- THIS DOES NOT WORK, DON'T DO THIS
+		}
+
+		if ( Input.GetKey(KeyCode.A) ) {
+		// all of these lines of code do similar things
+		//	transform.rotation *= Quaternion.Euler(0f, -5f, 0f); // this also works, but it's overkill
+		//	transform.eulerAngles += new Vector3( 0f, -5f, 0f ); // another way to turn left
+			transform.Rotate( 0f, -50f * Time.deltaTime, 0f ); // more intuitive way to turn left?
+		}
+
+		if ( Input.GetKey(KeyCode.D ) ) {
+			transform.Rotate( 0f, 50f * Time.deltaTime, 0f );
+		}
+
+		if ( Input.GetKey(KeyCode.S ) ) {
+			transform.Translate( 0f, 0f, -10f * Time.deltaTime );
+		}
+	}
+
+}
---
+++ week02/Assets/scripts/TapGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TapGame : MonoBehaviour {
+
+	int score = 0; // player's current score
+	Text myText;
+
+	// Use this for initialization
+	void Start () {
+		// assign and cache the reference to the Text component
+		myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// update text contents with current score (and game instructions?)
+		myText.text = "TAPPY GAME! TAP [SPACE] TO WIN\n\nCURRENT SCORE: " + score.ToString();
+
+		// add 1 point every time we tap Spacebar
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {
+//			score++;
+//			score = score + 1;
+			score += 1;
+		}
+
+	}
+}
