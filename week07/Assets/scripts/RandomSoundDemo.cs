df86580ae64f5e07cf096e41212cfeee68717da0
Robert Yang <yang.robert.w@gmail.com>
2016-10-21 01:04:48 -0400

    week07: sound and audio playback control, see SoundDemo.cs and RandomSoundDemo.cs

---
+++ week07/Assets/scripts/RandomSoundDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// example of playing a randomly picked sound
+public class RandomSoundDemo : MonoBehaviour {
+
+	public AudioClip[] arrayOfSounds; // all the possible sounds I could play
+	public AudioSource myAudioSource; // where the sound actually comes out of
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.R) ) { // if I press R...
+			// pick a random number based on how big the array is
+			int randomNumber = Random.Range( 0, arrayOfSounds.Length );
+			// use that number to play a random sound
+			myAudioSource.PlayOneShot( arrayOfSounds[randomNumber] );
+		}
+
+	}
+
+}
---
+++ week07/Assets/scripts/SoundDemo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDemo : MonoBehaviour {
+
+	public AudioSource myAudioSource; // assign in Inspector
+
+	void Update () {
+		// if the user presses SPACE...
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			// EXAMPLE 1, simple sound playback:
+			// myAudioSource.Play();
+
+			// USE CASE: triggered dialogue
+			// EXAMPLE 2, play a sound, but don't interrupt if already playing:
+//			if ( myAudioSource.isPlaying == false ) {
+//				myAudioSource.Play();
+//			}
+
+			// USE CASE: fast repeated sounds (gunshots, impact, footsteps)
+			// EXAMPLE 3, play a new "instance" of the sound, each time we press SPACE
+			// myAudioSource.PlayOneShot( myAudioSource.clip );
+
+			// EXAMPLE 4: toggle a looping sound
+			if ( myAudioSource.isPlaying == false ) { // is the sound already NOT looping?
+				myAudioSource.loop = true; // make sure it is set to automatically loop
+				myAudioSource.Play();
+			} else {
+				myAudioSource.loop = false;
+				myAudioSource.Stop();
+				// use Pause() and UnPause() to keep place
+			}
+
+
+
+		}
+	}
+
+}
